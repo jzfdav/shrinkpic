8caf0dc9fbd1bfc49f0b785088aeae817d68054b
Sreenath K <sreenk06@in.ibm.com>
2026-02-03 16:01:22 +0530

    Add aliases to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
+import preact from "@preact/preset-vite";
+import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      "@ui": resolve(resolve(__dirname), "./src/components/ui/"),
+      "@": resolve(resolve(__dirname), "./src/"),
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
