5ae86f5c660bdfca4f6f61046426357d33f412f5
Sreenath K <sreenk06@in.ibm.com>
2026-02-03 15:52:39 +0530

    Fix Vite plugin config typing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   base: "/shrinkpic/",
-  plugins: [preact()],
+  plugins: preact(),
   server: {
     host: true,
   },
